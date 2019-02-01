f45355fe936e750f7e76984fc910207d597dd5cf
Athmann <37657102+iamathman@users.noreply.github.com>
2019-02-02 02:49:54 +0300

    Add files via upload
    The face detection system uses openCV to identify faces in a live video feed.

--- detect.py
+++ detect.py
@@ -1,30 +1,20 @@
-import cv2
-import os, sys
+#this block identifies a face in a given picture by use of haarcascade defined in openCV
 import numpy as np
-import math
-from matplotlib import pyplot as plt 
+import cv2
 
-webcam = cv2.VideoCapture(0)
-webcam.open(0)
-folder = "people/" + raw_input('Person:').lower() #input name
-cv2.namedWindow("PyData Tutorial", cv2.WINDOW_AUTOSIZE)
-if not os.path.exists(folder):
-	os.mkdir(folder)
-	counter = 0
-	timer = 0
-	while counter < 10 : #take 10 pictures
-		frame = webcam.get_frame()
-		faces_coord = detector.detect(frame) #detect
-		if len(faces_coord) and timer % 700 == 50: #every second or so
-			faces = normalize_faces(frame, faces_coord) #norm pipeline
-			cv2.imwrite(folder + '/' + str(counter) + '.jpg' , faces[0])
-			plt_show(faces[0], "images saved:" + str(counter))
-			clear_output(wait = True)# saved face in notebook
-			counter += 1
-		draw_rectangle(frame, faces_coord) # rectangle around the face
-		cv2.imshow("Pydata tutorial", frame) #live feed in external
-		cv2.waitkey(50)
-		timer += 50
-	cv2.destroyAllWindows()
-else:
-	print "This name already exists"
+detector= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,69,255),2)
+
+    cv2.imshow('frame',img)
+    if cv2.waitKey(1) & 0xFF == ord('p'):#press p to terminate the window
+        break
+
+cap.release()
+cv2.destroyAllWindows()
